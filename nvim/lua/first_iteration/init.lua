5e1de2c1db7eee97d0e9de056a60c70096b4b083
Stefano Lecchi <stefanolecchi0@gmail.com>
2025-01-09 12:00:38 +0100

    new: disabled zig auto-format on write

--- nvim/lua/first_iteration/init.lua
+++ nvim/lua/first_iteration/init.lua
@@ -1,4 +1,4 @@
 require("first_iteration.set")
 require("first_iteration.remap")
 require("first_iteration.packer")
-
+require("first_iteration.languages_settings")
---
+++ nvim/lua/first_iteration/languages_settings/init.lua
@@ -0,0 +1,1 @@
+require("first_iteration.languages_settings.zig")
